154262a9b8ac76c1e14d51ffad7804a4c7d1c331
617301 <s.kubina@integra-computer.de>
2015-03-02 14:28:41 +0100

    Servlet um Session erweitert String wird mit jedem Aufruf erweitert

--- WinterSchool2015/src/org/hslu/winterschool2015/servlets/HalloWelt.java
+++ WinterSchool2015/src/org/hslu/winterschool2015/servlets/HalloWelt.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class HalloWelt
@@ -30,13 +31,27 @@ public class HalloWelt extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		PrintWriter meinWriter = response.getWriter();
+		HttpSession session = request.getSession(true);
+		
+		
+		String meinString = (String) session.getAttribute("print");
+		
+		if(meinString == null){
+			meinString = "";
+		}
+		
+		
 		
 		String meinParameter = request.getParameter("print");
+		
 		if(meinParameter == null){
 			meinParameter = "";
 		}
 		
+		meinString = meinString + meinParameter;
+		session.setAttribute("print", meinString);
+		
+		PrintWriter meinWriter = response.getWriter();
 		meinWriter.append("<html> \n");
 		
 			meinWriter.append("<head> \n");
@@ -44,17 +59,16 @@ public class HalloWelt extends HttpServlet {
 			
 			meinWriter.append("<body> \n");
 				meinWriter.append("Hallo Welt! \n");
-				meinWriter.append( (2 * 3) + "<br />" );
 				
 				meinWriter.append("<a href=\"HalloWelt?print=4\" >Print 4!</a> <br />");
 				meinWriter.append("<a href=\"HalloWelt?print=5\" >Print 5!</a> <br />");
-				meinWriter.append(meinParameter);
+				meinWriter.append(meinString);
 				
 			meinWriter.append("</body> \n");
 			
 		meinWriter.append("</html> \n");
 		
-		System.out.println("Hallo Welt!");
+		//System.out.println("Hallo Welt!");
 		
 		
 		
